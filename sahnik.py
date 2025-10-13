f3875e27a18b4ab81da196c8e0259ae8779ee291
Sahnik0 <sahnik.biswas@stu.adamasuniversity.ac.in>
2025-10-13 10:27:41 +0530

    first commit

---
+++ sahnik.py
@@ -0,0 +1,12 @@
+s1 = {1,2,3}
+s2 = {3,4,5}
+
+s1.add(6)
+s1.remove(2)
+print(s1.union(s2))
+print(s1.intersection(s2))
+print(s1.difference(s2))
+print(s1.issubset(s2))
+print(s1.isdisjoint(s2))
+print(s1.isdisjoint(s2))
+
